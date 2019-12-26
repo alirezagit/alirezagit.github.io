9e16649fd047367d6b9da627a21ab2ecacc320a6
alirezagit <47676068+alirezagit@users.noreply.github.com>
2019-12-26 16:29:14 +0330

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,22 @@
+$('#inputid').on('keyup', function(event) {
+    event.preventDefault();
+    var str = $('#inputid').val();
+$.ajax({
+   url: "https://docs.google.com/spreadsheets/d/14TYS9UBM5PETa2Y3_l68nwJZTjxjJkg9hMnOiizN3Gg/gviz/tq?tqx=out:html&tq=SELECT+B,+C,+D,+F,+G,+H+WHERE+H=" + "+" + "'" + str + "'",  
+   context: document.body
+   }).done(function(data) {
+      $('#new-projects').html(data);
+    	$('table').prop('id', 'userdata');
+      $('#inputname').val( $('#userdata').find('td:nth-child(1)').eq(1).html() );
+      $('#inputphone').val( $('#userdata').find('td:nth-child(2)').eq(1).html() );
+      $('#inputemail').val( $('#userdata').find('td:nth-child(3)').eq(1).html() );
+      $('#inputpost').val( $('#userdata').find('td:nth-child(4)').eq(1).html() );
+      $('#inputcity').val( $('#userdata').find('td:nth-child(5)').eq(1).html() );
+      $('#userid').val( $('#userdata').find('td:nth-child(6)').eq(1).html() );
+  });
+});
+
+function GFG_Fun() { 
+document.getElementById('userno').value = 
+Math.random().toString(36).slice(2); 
+}
