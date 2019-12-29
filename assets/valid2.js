3332cbbb4f90f47a9c5604da80f82f69c316be2d
alirezagit <47676068+alirezagit@users.noreply.github.com>
2019-12-29 15:13:46 +0330

    Add files via upload

---
+++ assets/valid2.js
@@ -0,0 +1,22 @@
+(function() {
+window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation2');
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+form.addEventListener('submit', function(event) {
+if (form.checkValidity() === false) {
+event.preventDefault();
+event.stopPropagation();
+}
+if (form.checkValidity() === true) {
+event.preventDefault(); // to prevent default page reloading
+	  $('#order-form').attr('hidden', 'true');
+	  $('#btn-btn').attr('hidden', 'true');
+      $('#user-order').removeAttr('hidden');
+}
+form.classList.add('was-validated');
+}, false);
+});
+}, false);
+})();
