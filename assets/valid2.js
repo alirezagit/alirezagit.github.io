ccb213d75370b3ebff28eb0812696beabf00c979
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-01-01 12:49:05 +0330

    Update valid2.js

--- assets/valid2.js
+++ assets/valid2.js
@@ -1,12 +1,10 @@
-$(function() {
-  $("#order-form").on("submit",function(e) {
-    event.preventDefault();
-    if ($("#inputname").val() == '') {
-      alert('لطفا کد کاربری را درست وارد کنید');
-      return false; // cancel submit
+$("#order-form").on("submit",function(e) {
+  event.preventDefault();
+  if ($("#inputname").val() == '') {
+    alert('لطفا کد کاربری را درست وارد کنید');
+    return false; // cancel submit
     } else
-      $('#order-form').attr('hidden', 'true');
-      $('#btn-btn').attr('hidden', 'true');
-      $('#user-order').removeAttr('hidden');
-  });
+    $('#order-form').attr('hidden', 'true');
+    $('#btn-btn').attr('hidden', 'true');
+    $('#user-order').removeAttr('hidden');
 });
