341f85be566ee08ea71fcbc3658c6fe395bcb5aa
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-01-19 10:53:36 +0330

    Update valid3.js

--- assets/valid3.js
+++ assets/valid3.js
@@ -1,3 +1,4 @@
+(function() {
 window.addEventListener('load', function() {
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
 var forms = document.getElementsByClassName('needs-validation4');
