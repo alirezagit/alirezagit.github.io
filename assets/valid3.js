3bb5d3c1e963b436d07d36ca5a44cc2cbb80aaf9
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-01-25 16:01:42 +0330

    Update valid3.js

--- assets/valid3.js
+++ assets/valid3.js
@@ -35,6 +35,13 @@ function Execc() {
 	location.reload()
 }
 
-function spin() {
-	$("spinner").removeAttr("hidden");
-}
+$(document).ready(function() {
+    $("#orderbtn").click(function() {
+      // disable button
+      $(this).prop("disabled", true);
+      // add spinner to button
+      $(this).html(
+        `<span class="spinner-border spinner-border-md" role="status" aria-hidden="true"></span>
+      );
+    });
+});
