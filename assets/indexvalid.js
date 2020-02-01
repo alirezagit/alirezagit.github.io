e21257e77f3da539560ab1a579468b4aa433f144
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-02-01 12:11:00 +0330

    Update indexvalid.js

--- assets/indexvalid.js
+++ assets/indexvalid.js
@@ -19,6 +19,7 @@ event.stopPropagation();
 }
 if (form.checkValidity() === true) {
 event.preventDefault(); // to prevent default page reloading
+ldbtn.toggle()
 var dataString = $(this).serialize(); // to get the form data
 $.ajax({
 type: "POST",
@@ -34,5 +35,6 @@ form.classList.add('was-validated');
 })();
 
 function toggleAlert() {
+	ldbtn.toggle()
 	alertify.success('پیام شما با موفقیت ارسال گردید. با تشکر');
 }
