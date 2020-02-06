49bb670cfef80cf70c077399adfd34d627724d4f
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-02-06 19:58:27 +0330

    Update indexvalid.js

--- assets/indexvalid.js
+++ assets/indexvalid.js
@@ -25,7 +25,7 @@ type: "POST",
 url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScAMlOpsQOXLjYqB22kPeqE7lcU1t6eUE9HHWJV0EyBQtbD4w/formResponse",
 data: dataString,
 beforeSend: ldbtn.toggle(),
-done: ldbtn.toggle()
+complete: ldbtn.toggle()
 });
 }
 form.classList.add('was-validated');
