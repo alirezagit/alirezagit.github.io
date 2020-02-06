30a4e55055bc503cd1cbc34a97e9ec9feb227440
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-02-06 18:37:22 +0330

    Update indexvalid.js

--- assets/indexvalid.js
+++ assets/indexvalid.js
@@ -25,9 +25,7 @@ $.ajax({
 type: "POST",
 url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScAMlOpsQOXLjYqB22kPeqE7lcU1t6eUE9HHWJV0EyBQtbD4w/formResponse",
 data: dataString,
-beforeSend: function() {
-ldbtn.toggle()
-},
+beforeSend: ldbtn.toggle(),
 complete: ldbtn.toggle()
 });
 }
