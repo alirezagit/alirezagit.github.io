8a2aa2352f25cfa9a8e6aa2572ca28c9604284ed
alirezagit <47676068+alirezagit@users.noreply.github.com>
2020-02-06 16:39:36 +0330

    Update indexvalid.js

--- assets/indexvalid.js
+++ assets/indexvalid.js
@@ -9,7 +9,6 @@
 'use strict';
 window.addEventListener('load', function() {
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
-var ldbtn = new ldLoader({root: "#submit-form"});
 var forms = document.getElementsByClassName('needs-validation3');
 // Loop over them and prevent submission
 var validation = Array.prototype.filter.call(forms, function(form) {
@@ -34,3 +33,5 @@ form.classList.add('was-validated');
 });
 }, false);
 })();
+
+var ldbtn = new ldLoader({root: "#submit-form"});
