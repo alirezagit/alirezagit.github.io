64afc18d64a97c3308994598201dd2a8c0aaecb0
Alireza <47676068+alirezagit@users.noreply.github.com>
2021-10-06 15:23:28 +0330

    Update index.js

--- donate/index.js
+++ donate/index.js
@@ -1,6 +1,6 @@
 // user details 
 const userInfo = {
- avatar: 'https://alirezagit.ir/IMG_20210903_164547.jpg', 
+ avatar: 'https://alirezagit.ir/avatar.jpg', 
  hero: 'https://wallpapercave.com/wp/YBqpqJ0.jpg', 
  website: 'https://alirezagit.ir/', 
  name: 'Alireza Soltanpour', 
